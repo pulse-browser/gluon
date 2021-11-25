0850ac07f055910cb79b532510afee91dfcbe589
trickypr <trickypr@icloud.com>
2021-11-25 16:43:54 +1100

    🐛 Fetch the correct verson from mozilla

--- src/middleware/update-check.ts
+++ src/middleware/update-check.ts
@@ -1,25 +1,19 @@
-import axios from 'axios'
 import { config, log } from '..'
+import { getLatestFF } from '../utils'
 
 export const updateCheck = async (): Promise<void> => {
   const firefoxVersion = config.version.version
 
   try {
-    const { data } = await axios.get(
-      `https://product-details.mozilla.org/1.0/firefox_history_major_releases.json`,
-      { timeout: 1000 }
-    )
+    const version = await getLatestFF(config.version.product)
 
-    if (data) {
-      const version = Object.keys(data)[Object.keys(data).length - 1]
-
-      if (firefoxVersion && version !== firefoxVersion)
-        log.warning(
-          `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}).`
-        )
-    }
+    if (firefoxVersion && version !== firefoxVersion)
+      log.warning(
+        `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}).`
+      )
   } catch (e) {
     log.warning(`Failed to check for updates.`)
     log.askForReport()
+    log.error(e)
   }
 }
