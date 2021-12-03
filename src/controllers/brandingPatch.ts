387a05ba0be2bcac0a9ab2afbaf821ee9221017a
trickypr <trickypr@icloud.com>
2021-12-03 16:05:31 +1100

    ♻️ Break branding patch into different functions

--- src/controllers/brandingPatch.ts
+++ src/controllers/brandingPatch.ts
@@ -5,7 +5,6 @@ import {
   readdirSync,
   readFileSync,
   rmdirSync,
-  rmSync,
   writeFileSync,
 } from 'fs'
 import { mkdirpSync } from 'fs-extra'
@@ -42,6 +41,10 @@ export class BrandingPatch extends PatchBase {
   }
 
   private checkForFaults(): void {
+    if (!existsSync(this.configPath)) {
+      throw new Error(`Branding ${this.name} does not exist`)
+    }
+
     const requiredFiles = ['logo.png'].map((file) =>
       join(this.configPath, file)
     )
@@ -62,10 +65,6 @@ export class BrandingPatch extends PatchBase {
     try {
       log.debug('Checking branding files')
 
-      if (!existsSync(this.configPath)) {
-        throw new Error(`Branding ${this.name} does not exist`)
-      }
-
       this.checkForFaults()
 
       const brandingConfig = {
@@ -79,84 +78,104 @@ export class BrandingPatch extends PatchBase {
         rmdirSync(this.outputPath, { recursive: true })
       mkdirSync(this.outputPath, { recursive: true })
 
-      log.debug('Creating default*.png files')
-
-      for (const size of [16, 22, 24, 32, 48, 64, 128, 256]) {
-        await sharp(join(this.configPath, 'logo.png'))
-          .resize(size, size)
-          .toFile(join(this.outputPath, `default${size}.png`))
-      }
-
-      log.debug('Creating firefox*.ico')
-
-      await sharp(join(this.configPath, 'logo.png'))
-        .resize(512, 512)
-        .toFile(join(this.outputPath, 'firefox.ico'))
-      await sharp(join(this.configPath, 'logo.png'))
-        .resize(64, 64)
-        .toFile(join(this.outputPath, 'firefox64.ico'))
-
-      log.debug('Creating content/about-logo*.png')
-
-      mkdirSync(join(this.outputPath, 'content'), { recursive: true })
-
-      await sharp(join(this.configPath, 'logo.png'))
-        .resize(512, 512)
-        .toFile(join(this.outputPath, 'content', 'about-logo.png'))
-      await sharp(join(this.configPath, 'logo.png'))
-        .resize(1024, 1024)
-        .toFile(join(this.outputPath, 'content', 'about-logo@2x.png'))
+      await this.setupImages()
 
       log.debug(`Setup locales`)
 
-      readdirSync(join(templateDir, 'branding.optional'))
-        .map((file) => [
-          readFileSync(join(templateDir, 'branding.optional', file), 'utf-8'),
-          join(this.outputPath, 'locales/en-US', file),
-        ])
-        .forEach(([contents, path]) => {
-          mkdirSync(dirname(path), { recursive: true })
-          writeFileSync(path, stringTemplate(contents, brandingConfig))
-        })
+      this.setupLocales(brandingConfig)
 
       log.debug(`Copying files from ${BRANDING_FF}`)
 
-      const files = (await walkDirectory(BRANDING_FF)).filter(
-        (file) =>
-          !existsSync(join(this.outputPath, file.replace(BRANDING_FF, '')))
-      )
-
-      const css = files.filter((file) => extname(file).includes('css'))
-
-      const everythingElse = files.filter((file) => !css.includes(file))
-
-      css
-        .map((filePath) => [
-          readFileSync(filePath, 'utf-8'),
-          join(this.outputPath, filePath.replace(BRANDING_FF, '')),
-        ])
-        .map(([contents, path]) => [
-          contents.replace(CSS_REPLACE_REGEX, 'var(--theme-bg)') +
-            `:root { --theme-bg: ${brandingConfig.backgroundColor} }`,
-          path,
-        ])
-        .forEach(([contents, path]) => {
-          mkdirSync(dirname(path), { recursive: true })
-          writeFileSync(path, contents)
-        })
-
-      // Copy everything else from the default firefox branding directory
-      everythingElse.forEach((file) => {
-        mkdirpSync(
-          dirname(join(this.outputPath, file.replace(BRANDING_FF, '')))
-        )
-        copyFileSync(file, join(this.outputPath, file.replace(BRANDING_FF, '')))
-      })
+      await this.copyMozillaFiles(brandingConfig)
 
       this.done = true
     } catch (e) {
       this.error = e
       this.done = false
     }
+  }
+
+  private async copyMozillaFiles(brandingConfig: {
+    backgroundColor: string
+    brandShorterName: string
+    brandShortName: string
+    brandFullName: string
+  }) {
+    const files = (await walkDirectory(BRANDING_FF)).filter(
+      (file) =>
+        !existsSync(join(this.outputPath, file.replace(BRANDING_FF, '')))
+    )
+
+    const css = files.filter((file) => extname(file).includes('css'))
+
+    const everythingElse = files.filter((file) => !css.includes(file))
+
+    css
+      .map((filePath) => [
+        readFileSync(filePath, 'utf-8'),
+        join(this.outputPath, filePath.replace(BRANDING_FF, '')),
+      ])
+      .map(([contents, path]) => [
+        contents.replace(CSS_REPLACE_REGEX, 'var(--theme-bg)') +
+          `:root { --theme-bg: ${brandingConfig.backgroundColor} }`,
+        path,
+      ])
+      .forEach(([contents, path]) => {
+        mkdirSync(dirname(path), { recursive: true })
+        writeFileSync(path, contents)
+      })
+
+    // Copy everything else from the default firefox branding directory
+    everythingElse.forEach((file) => {
+      mkdirpSync(dirname(join(this.outputPath, file.replace(BRANDING_FF, ''))))
+      copyFileSync(file, join(this.outputPath, file.replace(BRANDING_FF, '')))
+    })
+  }
+
+  private setupLocales(brandingConfig: {
+    backgroundColor: string
+    brandShorterName: string
+    brandShortName: string
+    brandFullName: string
+  }) {
+    readdirSync(join(templateDir, 'branding.optional'))
+      .map((file) => [
+        readFileSync(join(templateDir, 'branding.optional', file), 'utf-8'),
+        join(this.outputPath, 'locales/en-US', file),
+      ])
+      .forEach(([contents, path]) => {
+        mkdirSync(dirname(path), { recursive: true })
+        writeFileSync(path, stringTemplate(contents, brandingConfig))
+      })
+  }
+
+  private async setupImages() {
+    log.debug('Creating default*.png files')
+
+    for (const size of [16, 22, 24, 32, 48, 64, 128, 256]) {
+      await sharp(join(this.configPath, 'logo.png'))
+        .resize(size, size)
+        .toFile(join(this.outputPath, `default${size}.png`))
+    }
+
+    log.debug('Creating firefox*.ico')
+
+    await sharp(join(this.configPath, 'logo.png'))
+      .resize(512, 512)
+      .toFile(join(this.outputPath, 'firefox.ico'))
+    await sharp(join(this.configPath, 'logo.png'))
+      .resize(64, 64)
+      .toFile(join(this.outputPath, 'firefox64.ico'))
+
+    log.debug('Creating content/about-logo*.png')
+
+    mkdirSync(join(this.outputPath, 'content'), { recursive: true })
+
+    await sharp(join(this.configPath, 'logo.png'))
+      .resize(512, 512)
+      .toFile(join(this.outputPath, 'content', 'about-logo.png'))
+    await sharp(join(this.configPath, 'logo.png'))
+      .resize(1024, 1024)
+      .toFile(join(this.outputPath, 'content', 'about-logo@2x.png'))
   }
 }
