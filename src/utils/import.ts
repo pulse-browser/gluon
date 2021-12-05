b6dca8257e862ac81b272ffbe3d531ad2820d48f
trickypr <trickypr@icloud.com>
2021-12-06 10:47:39 +1100

    🐛 Go back to fs-extra for symlinks Windows doesn't like nodejs's defualt symlink implementation

--- src/utils/import.ts
+++ src/utils/import.ts
@@ -1,8 +1,9 @@
 import { existsSync } from 'fs'
 import { lstatSync, readFileSync } from 'fs'
+import { ensureSymlink } from 'fs-extra'
 import { resolve } from 'path'
 import rimraf from 'rimraf'
-import { appendToFileSync, createSymlink } from '.'
+import { appendToFileSync } from '.'
 import { ENGINE_DIR, SRC_DIR } from '../constants'
 
 const getChunked = (location: string) => location.replace(/\\/g, '/').split('/')
@@ -11,35 +12,28 @@ export const copyManual = async (
   name: string,
   noIgnore?: boolean
 ): Promise<void> => {
-  try {
-    // If the file exists and is not a symlink, we want to replace it with a
-    // symlink to our file, so remove it
-    if (
-      existsSync(resolve(ENGINE_DIR, ...getChunked(name))) &&
-      !lstatSync(resolve(ENGINE_DIR, ...getChunked(name))).isSymbolicLink()
-    ) {
-      rimraf.sync(resolve(ENGINE_DIR, ...getChunked(name)))
-    }
+  // If the file exists and is not a symlink, we want to replace it with a
+  // symlink to our file, so remove it
+  if (
+    existsSync(resolve(ENGINE_DIR, ...getChunked(name))) &&
+    !lstatSync(resolve(ENGINE_DIR, ...getChunked(name))).isSymbolicLink()
+  ) {
+    rimraf.sync(resolve(ENGINE_DIR, ...getChunked(name)))
+  }
 
-    // Create the symlink
-    await createSymlink(
-      resolve(SRC_DIR, ...getChunked(name)),
-      resolve(ENGINE_DIR, ...getChunked(name))
-    )
+  // Create the symlink
+  await ensureSymlink(
+    resolve(SRC_DIR, ...getChunked(name)),
+    resolve(ENGINE_DIR, ...getChunked(name))
+  )
 
-    if (!noIgnore) {
-      const gitignore = readFileSync(resolve(ENGINE_DIR, '.gitignore'), 'utf-8')
+  if (!noIgnore) {
+    const gitignore = readFileSync(resolve(ENGINE_DIR, '.gitignore'), 'utf-8')
 
-      if (!gitignore.includes(getChunked(name).join('/')))
-        appendToFileSync(
-          resolve(ENGINE_DIR, '.gitignore'),
-          `\n${getChunked(name).join('/')}`
-        )
-    }
-
-    return
-  } catch (e) {
-    console.log(e)
-    process.exit(0)
+    if (!gitignore.includes(getChunked(name).join('/')))
+      appendToFileSync(
+        resolve(ENGINE_DIR, '.gitignore'),
+        `\n${getChunked(name).join('/')}`
+      )
   }
 }
