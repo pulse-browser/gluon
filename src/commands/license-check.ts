6ca741c64d6459cd00d8e1386036b62b5ddf1aa4
trickypr <trickypr@icloud.com>
2022-01-21 14:02:42 +1100

    ✨ Add auto fixing

--- src/commands/license-check.ts
+++ src/commands/license-check.ts
@@ -1,5 +1,6 @@
-import { readFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
 import Listr, { ListrTask } from 'listr'
+import { join } from 'path'
 
 import { SRC_DIR } from '../constants'
 import { walkDirectory } from '../utils'
@@ -8,7 +9,7 @@ const ignoredFiles = new RegExp('.*\\.(json|patch|md)')
 const licenseIgnore = new RegExp('(//) Ignore license in this file', 'g')
 const fixableFiles = [{ regex: new RegExp('.*\\.js'), comment: '// ' }]
 
-export function checkFile(path: string): ListrTask<any> {
+export function checkFile(path: string, noFix: boolean): ListrTask<any> {
   return {
     skip: () => ignoredFiles.test(path),
     title: path.replace(SRC_DIR, ''),
@@ -23,19 +24,40 @@ export function checkFile(path: string): ListrTask<any> {
         licenseIgnore.test(contents.join('\n'))
 
       if (!hasLicense) {
-        throw new Error(
-          `${path} does not have a license. Please add the source code header`
-        )
+        const fixable = fixableFiles.find(({ regex }) => regex.test(path))
+
+        if (!fixable || noFix) {
+          throw new Error(
+            `${path} does not have a license. Please add the source code header`
+          )
+        } else {
+          const mpl = await readFile(
+            join(__dirname, 'license-check.txt'),
+            'utf8'
+          )
+          const { comment } = fixable
+
+          const header = mpl
+            .split('\n')
+            .map((ln) => comment + ln)
+            .join('\n')
+
+          await writeFile(path, header + '\n' + contents.join('\n'))
+        }
       }
     },
   }
 }
 
-export const licenseCheck = async (): Promise<void> => {
+interface Options {
+  noFix: boolean
+}
+
+export const licenseCheck = async (options: Options): Promise<void> => {
   const files = await walkDirectory(SRC_DIR)
 
   await new Listr(
-    files.map((file) => checkFile(file)),
+    files.map((file) => checkFile(file, options.noFix)),
     {
       concurrent: true,
       exitOnError: false,
