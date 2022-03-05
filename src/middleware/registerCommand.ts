1bc68b822ac567b68ac02f84ec2abd8f68cd47b7
trickypr <trickypr@icloud.com>
2022-03-05 17:17:40 +1100

    📝  Document a midleware function

--- src/middleware/registerCommand.ts
+++ src/middleware/registerCommand.ts
@@ -1,6 +1,13 @@
 import { writeFileSync } from 'fs'
 import { resolve } from 'path'
+import { MELON_DIR } from '../constants'
 
+/**
+ * Stores the name of the current command on the file system to be accessed if
+ * the command crashes to provide more helpful error reporting
+ *
+ * @param command The name of the command about to be run
+ */
 export function registerCommand(command: string): void {
-  writeFileSync(resolve(process.cwd(), '.dotbuild', 'command'), command)
+  writeFileSync(resolve(MELON_DIR, 'command'), command)
 }
