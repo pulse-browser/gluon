7d59b512600f6fe62460876efcb254c640ee8839
trickypr <trickypr@icloud.com>
2022-05-08 13:28:41 +1000

    🔥  Do not provide a custom boostraper

--- src/commands/bootstrap/macos.ts
+++
@@ -1,8 +0,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-const brewDependencies = [
-  'gnu-tar', // MacOS tar doesn't support the --transform option
-]
-
-export async function bootstrapMacos() {}
