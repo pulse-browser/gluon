5d94097653c51eee94ffd0ea581f21c0db48c21b
SplatboyDev <splatboy20081@gmail.com>
2022-10-28 18:18:49 +0200

    🩹Update check advises you to run update

--- src/middleware/update-check.ts
+++ src/middleware/update-check.ts
@@ -13,7 +13,7 @@ export const updateCheck = async (): Promise<void> => {
 
     if (firefoxVersion && version !== firefoxVersion)
       log.warning(
-        `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}).`
+        `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}). Update Firefox with the command |gluon update|.`
       )
   } catch (error) {
     log.warning(`Failed to check for updates.`)
