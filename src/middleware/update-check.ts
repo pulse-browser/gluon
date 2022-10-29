345aa37bde9df699f818de2be01db246a2ffe262
SplatboyDev <splatboy20081@gmail.com>
2022-10-29 12:15:48 +0200

    Use bin_name

--- src/middleware/update-check.ts
+++ src/middleware/update-check.ts
@@ -1,7 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import { config } from '..'
+import { bin_name, config } from '..'
 import { log } from '../log'
 import { getLatestFF } from '../utils'
 
@@ -13,7 +13,7 @@ export const updateCheck = async (): Promise<void> => {
 
     if (firefoxVersion && version !== firefoxVersion)
       log.warning(
-        `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}). Update Firefox with the command |gluon update|.`
+        `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}). Update Firefox with the command |${bin_name} update|.`
       )
   } catch (error) {
     log.warning(`Failed to check for updates.`)
